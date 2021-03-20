3fc01dfde89c5ec2a3f66248361da81ac90d4f04
adrianavv1 <adrianavaldiglesias2@gmail.com>
2021-03-19 23:41:43 -0400

    added components folder, filter function for employees by last name

---
+++ src/components/Row.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Row(props) {
+    return (
+        <>
+            <tr>
+                <th scope="row">1</th>
+                <td>{props.employee.name.first}</td>
+                <td>{props.employee.name.last}</td>
+                <td>{props.employee.email}</td>
+             </tr>
+        </>
+    );
+}
+
+export default Row;
---
+++ src/components/Table.js
@@ -0,0 +1,55 @@
+import React, {useState, useEffect} from 'react';
+import Row from './Row';
+
+
+function Table(props) {
+    const [employeeList, setEmployeeList] = useState([])
+    const [filteredEmployeeList, setFilteredEmployeeList] = useState([])
+
+    useEffect(() => {
+        fetch('https://randomuser.me/api/?results=10')
+        .then(response => response.json())
+        .then(res => {
+            console.log(res)
+            setEmployeeList(res.results);
+            setFilteredEmployeeList(res.results)
+        })
+    }, [])
+
+    function filter(e) {
+        let newList =employeeList.filter((employee) => {
+            return employee.name.last.includes(e.target.value)
+        })
+
+        setFilteredEmployeeList(newList);
+    }
+
+    return (
+        <>
+            <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" onChange={filter}/>
+            </div>
+            </nav>
+            <div>
+                <table class="table">
+                <thead>
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        filteredEmployeeList.map((employee) => <Row employee={employee} />)
+                    }
+                </tbody>
+                </table>
+            </div>
+        </>
+    );
+}
+
+export default Table;
