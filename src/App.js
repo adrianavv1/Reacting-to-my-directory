7d57f358099875c4ef665a8f4c802ef5d10f6fbf
adrianavv1 <adrianavaldiglesias2@gmail.com>
2021-04-13 21:59:02 -0400

    rebuilt sorting and filtering with material ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-// import logo from './logo.svg';
 import './App.css';
-import Table from './components/Table/Table'
+
+import Index from "./pages/index"
 
 function App() {
   return (
-    <div>
-      <Table />
+    <div className="App">
+      <Index />
     </div>
   );
 }
+
 
 export default App;
